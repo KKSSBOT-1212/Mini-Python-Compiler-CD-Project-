8e581e8fc16a474a696d56263031938a9774e31d
Krishnakanth S S <77207045+KKSSBOT-1212@users.noreply.github.com>
2021-09-02 22:33:44 +0530

    Add files via upload

---
+++ i2.py
@@ -0,0 +1,20 @@
+#Basic Code
+
+import scipy
+
+x = 2
+y = 1
+
+a = 3
+b = 4
+d = a+b
+
+if(x==1):
+	c=1
+elif(y==1):
+	c=2
+else:
+	c=1
+
+print(b)
+print(d)
---
+++ i8.py
@@ -0,0 +1,9 @@
+x=2
+y=1
+
+if(x==y):
+	c=1
+else:
+	c=2
+
+print(c)
